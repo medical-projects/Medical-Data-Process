d67cf7f6c6daa51fb9d4478ebb155fb7b242f6a3
SerdarHELLI <62758782+SerdarHelli@users.noreply.github.com>
2020-08-11 22:56:49 +0300

    Update medical_data_preparing_for_deep_learning.py

--- medical_data_preparing_for_deep_learning.py
+++ medical_data_preparing_for_deep_learning.py
@@ -4,12 +4,57 @@ Created on Sat Aug  8 01:38:13 2020
 
 @author: serdarhelli
 """
-import numpy as np
-import os 
-import numpy as np
-from PIL import Image
-x=np.load("C:/Users/sserd/Desktop/PROJE-DENTIST/x_train.npy")
-def cross_validator(x,fold,w,h,c,filepath):
+
+#extension for example "png", "jpeg"
+#please use like in filepath or savepath :  "..../"
+#w width h height c canal 
+def medical_image_resize(filepath,size,savepath,extension):#resizing all images
+    from PIL import Image
+    import os 
+    import numpy as np
+    dirs=sorted(os.listdir(filepath),key=len)
+    for i in range (0,len(dirs)):
+        img=Image.open(filepath+dirs[i])
+        (img.resize((size,size),Image.ANTIALIAS)).save(savepath+np.str(i+1)+"."+extension,extension, optimize=True)
+   
+
+
+#RGB if True : fuction vectorize images with RGB canal
+#all images must be same size
+def medical_data_prepare(filepath,RGB,savepath):#vectorize all images
+    import os 
+    import numpy as np
+    from PIL import Image
+    dirs=sorted(os.listdir(filepath),key=len)
+    img_x_train=Image.open(filepath+dirs[1])
+    w,h=img_x_train.size
+    img_x_train=np.asarray(img_x_train)
+    img_x_train1=np.asarray(Image.open(filepath+dirs[2]))
+    if RGB==False:
+        canal=1
+        x_train=np.concatenate((img_x_train[:,:,0],img_x_train1[:,:,0]),axis=None)
+        for i in range (2,len(dirs)):
+            img=np.asarray(Image.open(filepath+dirs[i]))
+            if np.size(img.shape)==3:
+                img=img[:,:,0]
+                x_train=np.concatenate((x_train,img),axis=None)
+            else:
+                x_train=np.concatenate((x_train,img),axis=None)
+    else :
+        x_train=np.concatenate((img_x_train,img_x_train1),axis=None)
+        canal=3
+        for i in range (2,len(dirs)):
+            img=np.asarray(Image.open(filepath+dirs[i]))
+            x_train=np.concatenate((x_train,img),axis=None)
+    x_train=np.reshape(x_train,(len(dirs),w,h,canal))
+    np.save(savepath+"Data.npy",x_train)
+
+
+
+
+
+
+def cross_validator(x,fold,w,h,c,filepath):#cross validator
     import numpy as np
     r=len(x)%fold
     testshape=len(x)//fold
@@ -38,54 +83,6 @@ def cross_validator(x,fold,w,h,c,filepath):
     np.save(filepath+"train"+np.str(fold)+".npy",x_train)
     return 
 
-
-cross_validator(x, 11, 512, 512, 3, "C:/Users/sserd/Desktop/deneme/")
-
-def medical_data_prepare(filepath,RGB,savepath):#all images must be same size
-    import os 
-    import numpy as np
-    from PIL import Image
-    dirs=sorted(os.listdir(filepath),key=len)
-    img_x_train=Image.open(filepath+dirs[1])
-    w,h=img_x_train.size
-    img_x_train=np.asarray(img_x_train)
-    img_x_train1=np.asarray(Image.open(filepath+dirs[2]))
-    if RGB==False:
-        canal=1
-        x_train=np.concatenate((img_x_train[:,:,0],img_x_train1[:,:,0]),axis=None)
-        for i in range (2,len(dirs)):
-            img=np.asarray(Image.open(filepath+dirs[i]))
-            if np.size(img.shape)==3:
-                img=img[:,:,0]
-                x_train=np.concatenate((x_train,img),axis=None)
-            else:
-                x_train=np.concatenate((x_train,img),axis=None)
-    else :
-        x_train=np.concatenate((img_x_train,img_x_train1),axis=None)
-        canal=3
-        for i in range (2,len(dirs)):
-            img=np.asarray(Image.open(filepath+dirs[i]))
-            x_train=np.concatenate((x_train,img),axis=None)
-    x_train=np.reshape(x_train,(len(dirs),w,h,canal))
-    np.save(savepath+"Data.npy",x_train)
-
-medical_data_prepare("C:/Users/sserd/Desktop/7TP-DATASET-EDIT/",True,"C:/Users/sserd/Desktop/")
-
-
-def medical_image_resize(filepath,size,savepath,extension):
-    from PIL import Image
-    import os 
-    import numpy as np
-    dirs=sorted(os.listdir(filepath),key=len)
-    for i in range (0,len(dirs)):
-        img=Image.open(filepath+dirs[i])
-        (img.resize((size,size),Image.ANTIALIAS)).save(savepath+np.str(i+1)+"."+extension,extension, optimize=True)
-   
-
-
-
-
-medical_image_resize("C:/Users/sserd/Desktop/PROJE-DENTIST/7TP-Dataset-23072020/",512,"C:/Users/sserd/Desktop/deneme","jpeg")
 
 
 
